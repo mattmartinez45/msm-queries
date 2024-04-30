796c8c7e574933c55626a3c52c2f307c2add11b3
mattmartinez45 <114887556+mattmartinez45@users.noreply.github.com>
2024-04-30 01:07:55 +0000

    more edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
+  
   get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/:young", {:controller => "directors", :action => "youngest"})
 
   get("/directors/:the_id", { :controller => "directors", :action => "show"})
 
